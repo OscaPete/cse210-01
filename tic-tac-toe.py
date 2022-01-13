939148b7930c8881690f4a36650e83ffc362bbcd
OscaPete <pet21048@byui.edu>
2022-01-12 22:47:42 -0700

    Ver. 2 removed winner statement, not functioning

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,18 +4,14 @@ def main() :
     
     win_status = game_is_won(board)
 
-    while win_status == "n/a" :
+    while not (game_is_won(board) or game_is_draw(board)) :
         show_board(board)
         make_move(current_player, board)
         current_player = next_player(current_player)
 
-    if win_status == "x" :
-        show_board(board)
-        print("Good game! Player x won!")
+    show_board(board)
+    print("GGs, feel free to now brag about winning to your opponent!")
 
-    elif win_status == "o" :
-        show_board(board)
-        print("Good game! Player o won!")
 
 def make_board() :
     board = []
@@ -25,12 +21,9 @@ def make_board() :
     return board
 
 def show_board(board) :
-    print(f"\n0{board[0]}|0{board[1]}|0{board[2]}|0{board[3]}")
-    print("--+--+--+--")
-    print(f"0{board[4]}|0{board[5]}|0{board[6]}|0{board[7]}")
-    print("--+--+--+--")
-    print(f"0{board[8]}|{board[9]}|{board[10]}|{board[11]}")
-    print("--+--+--+--")
+    print(f"\n{board[0]}|{board[1]}|{board[2]}|{board[3]}")
+    print(f"{board[4]}|{board[5]}|{board[6]}|{board[7]}")
+    print(f"{board[8]}|{board[9]}|{board[10]}|{board[11]}")
     print(f"{board[12]}|{board[13]}|{board[14]}|{board[15]}\n")
 
 def game_is_draw(board) :
@@ -40,14 +33,7 @@ def game_is_draw(board) :
     return True
 
 def game_is_won(space) :
-    if space[0] == space[1] == space[2] == space[3] == "x" or space[4] == space[5] == space[6] == space[7] == "x" or space[8] == space[9] == space[10] == space[11] == "x" or space[12] == space[13] == space[14] == space[15] == "x" or space[0] == space[4] == space[8] == space[12] == "x" or space[1] == space[5] == space[9] == space[13] == "x" or space[2] == space[6] == space[10] == space[14] == "x" or space[3] == space[7] == space[11] == space[15] == "x" or space[0] == space[5] == space[10] == space[15] == "x" or space[3] == space[6] == space[9] == space[12] == "x" :
-        return "x"
-
-    elif space[0] == space[1] == space[2] == space[3] == "o" or space[4] == space[5] == space[6] == space[7] == "o" or space[8] == space[9] == space[10] == space[11] == "o" or space[12] == space[13] == space[14] == space[15] == "o" or space[0] == space[4] == space[8] == space[12] == "o" or space[1] == space[5] == space[9] == space[13] == "o" or space[2] == space[6] == space[10] == space[14] == "o" or space[3] == space[7] == space[11] == space[15] == "o" or space[0] == space[5] == space[10] == space[15] == "o" or space[3] == space[6] == space[9] == space[12] == "o" :
-        return "o"
-
-    else :
-        return "n/a"
+    return (space[0] == space[1] == space[2] == space[3] or space[4] == space[5] == space[6] == space[7] or space[8] == space[9] == space[10] == space[11] or space[12] == space[13] == space[14] == space[15] or space[0] == space[4] == space[8] == space[12] or space[1] == space[5] == space[9] == space[13] or space[2] == space[6] == space[10] == space[14] or space[3] == space[7] == space[11] == space[15] or space[0] == space[5] == space[10] == space[15] or space[3] == space[6] == space[9] == space[12])
 
 def make_move(player, board) :
     space = int(input(f"{player}'s turn to choose a space (1-16)"))
